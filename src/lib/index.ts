53f7c209167d3aa67c86af4d13f83d1a80edaf77
SurajSSingh <22825536+SurajSSingh@users.noreply.github.com>
2024-12-24 17:33:25 -0800

    Load test data in homepage

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,5 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+import test from "./private.json" with { type: "json" };
+
+export let testData = test;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
+<script lang="ts">
+  import { testData } from "../lib";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>
+
+<p>
+  {JSON.stringify(testData)}
+</p>
