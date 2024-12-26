2d404bd11d5b51d7464cd2c05c8c13c6a3d4a314
SurajSSingh <22825536+SurajSSingh@users.noreply.github.com>
2024-12-25 16:47:08 -0800

    update index.ts with new user data

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,4 +2,9 @@
 
 import test from "./private.json" with { type: "json" };
 
+import staticData from "../private/static.json";
+import skillsData from "../private/dyanmic/skills.json";
+import expData from "../private/dyanmic/experience.json";
+
 export const testData = test;
+export const userData = { ...staticData, ...skillsData, experiennces: expData };
